e1a8fd6216e31a3aeb10e9af0befc1050c1fb1f4
Aditya Mandil <46640551+mandiladitya@users.noreply.github.com>
2021-05-09 09:42:13 +0530

    Update Connection.php

--- Database/Connection.php
+++ Database/Connection.php
@@ -1,8 +1,8 @@
 <?php
-$server="sql6.freemysqlhosting.net";
-$username="sql6409564";
-$password="MGAJFvvNxd";
-$dbname="sql6409564";
+$server="remotemysql.com:3306";
+$username="p0wflbg6M6";
+$password="48juAsxqI1";
+$dbname="p0wflbg6M6";
 //$con=mysqli_connect('sql6.freemysqlhosting.net','sql6409564','MGAJFvvNxd','sql6409564');
 $con=mysqli_connect($server,$username,$password,$dbname);
 ?>
